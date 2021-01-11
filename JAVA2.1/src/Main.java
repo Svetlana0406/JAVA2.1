391f940e709a5b9906abe5ea95107e4cb267a159
Svetlana0406 <75991471+Svetlana0406@users.noreply.github.com>
2021-01-11 19:54:40 +0300

    Add files via upload

---
+++ JAVA2.1/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int CurrentBalance = 2_000_000_000;
+        int Transfer = 500_000_000;
+        int FinalBalance = CurrentBalance + Transfer;
+        System.out.println(FinalBalance);
+    }
+}
